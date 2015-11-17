593e5d02eaa95606215bae214cf8229641fd9a4f
miaoweihong <miaoweihong@ipin.com>
2015-11-17 15:06:19 +0800

    change style

--- removeDuplicates.cpp
+++ removeDuplicates.cpp
@@ -1,15 +1,15 @@
 // 给一个有序数列，删除重复的元素
 //
 class Solution{
-		public:
-				int removeDuplicates(int A[],int n){
-						if(!n)
-								return 0;
-						int ret=1;
-						for(int i=1;i<n;i++){
-								if(A[i]!=A[i-1])
-										A[ret++]=A[i];
-						}
-						return ret;
-				}
+    public:
+    int removeDuplicates(int A[],int n){
+        if(!n)
+        return 0;
+        int ret=1;
+        for(int i=1;i<n;i++){
+            if(A[i]!=A[i-1])
+            A[ret++]=A[i];
+        }
+        return ret;
+    }
 };
